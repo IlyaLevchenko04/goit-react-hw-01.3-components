6e6c3ace1bb202d97d41b5d8527e0506d7b4d291
IlyaLevchenko04 <nneo2086@gmail.com>
2023-02-22 01:35:28 +0200

    r

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-console.log('hello world')
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
